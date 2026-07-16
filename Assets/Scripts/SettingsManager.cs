dfa3852108f66026655a14ef73408c479e38e9c7
exev <>
2026-07-16 11:04:15 +0300

    Pause

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -8,6 +8,8 @@ public class SettingsManager : MonoBehaviour
     public GameObject openSettingsCanvas;
     public GameObject closeSettingsCanvas;
 
+    public static bool isPaused; //for pausing the game
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,11 +25,16 @@ public class SettingsManager : MonoBehaviour
     public void OpenSettings()
     {
         openSettingsCanvas.SetActive(true);
+        Time.timeScale = 0f; //pauses the game
+        isPaused = true; //tells the code the game paused
     }
 
     public void CloseSettings()
     {
         openSettingsCanvas.SetActive(false);
+        Time.timeScale = 1f; //resumes the game to run
+        isPaused = false;//tells the code the game is not paused
+
     }
 
 }
